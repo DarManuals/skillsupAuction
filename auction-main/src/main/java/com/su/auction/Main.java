7285e8df7443cf2e704f60b4643f3590f28c9128
Bogdan <dar-bodya@i.ua>
2016-09-22 20:45:25 +0300

    bids history is added

--- auction-main/src/main/java/com/su/auction/Main.java
+++ auction-main/src/main/java/com/su/auction/Main.java
@@ -16,7 +16,6 @@ import org.springframework.context.annotation.Configuration;
 
 import java.math.BigDecimal;
 import java.text.SimpleDateFormat;
-import java.util.stream.IntStream;
 
 /**
  * Created by bogdan on 17.09.16.
